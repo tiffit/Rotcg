c1241fcb76623a5cf9f36bcf072e7918639ada6e
tiffit <10173431+tiffit@users.noreply.github.com>
2022-10-17 21:51:02 -0700

    Added showing loot in nearby containers

--- src/main/java/net/tiffit/rotcg/render/hud/info/HInfoNearbyInteractable.java
+++ src/main/java/net/tiffit/rotcg/render/hud/info/HInfoNearbyInteractable.java
@@ -7,7 +7,11 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiComponent;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
+import net.tiffit.realmnetapi.assets.xml.GameObject;
+import net.tiffit.realmnetapi.assets.xml.XMLLoader;
+import net.tiffit.realmnetapi.map.object.GameObjectState;
 import net.tiffit.realmnetapi.map.object.RObject;
+import net.tiffit.realmnetapi.map.object.StatType;
 import net.tiffit.realmnetapi.net.RealmNetworker;
 import net.tiffit.rotcg.Constants;
 import net.tiffit.rotcg.KeybindingManager;
@@ -74,16 +78,44 @@ public class HInfoNearbyInteractable {
             data.increasePosY(35);
         }
         if(nearbyContainers.size() > 0){
+            List<GameObject> items = new ArrayList<>();
+
             stack.pushPose();
             stack.translate(5, data.getPosY(), 0);
-            for (int i = 0; i < nearbyContainers.size(); i++) {
+            int colorDiff = 3;
+            int containerCount = nearbyContainers.size();
+            for (int i = 0; i < containerCount; i++) {
                 RotcgEntity containerEntity = nearbyContainers.get(i);
                 RenderSystem.setShaderTexture(0, RotCGPack.textToRlFull(containerEntity.animationManager.getTexture()));
-                int colorDiff = nearbyContainers.size() > 3 ? 0 : 3;
-                float color = (i + colorDiff) / (float)(nearbyContainers.size() + colorDiff);
+                float color = (i + colorDiff) / (float)(containerCount + colorDiff);
                 RenderSystem.setShaderColor(color, color, color, 1f);
                 GuiComponent.blit(stack, i*8, -5, 32, 32, 0, 0, 16, 16, 16, 16);
+                for(int j = StatType.INVENTORY_0.id; j <= StatType.INVENTORY_7.id; j++) {
+                    GameObjectState state = containerEntity.getReference().getState();
+                    int itemid = state.<Integer>getStat(StatType.byID(j));
+                    if(itemid == -1)continue;
+                    items.add(XMLLoader.OBJECTS.get(itemid));
+                }
             }
+            stack.pushPose();
+            stack.translate(32 + containerCount*8 + 5, -5, 0);
+            int numItems = Math.min(items.size(), 8*4);
+            int numRows = numItems <= 4 ? 1 : numItems <= 8 ? 2 : 4;
+            int drawSize = numRows == 1 ? 32 : numRows == 2 ? 16 : 8;
+            for (int i = 0; i < numItems; i++) {
+                GameObject go = items.get(i);
+                RenderSystem.setShaderTexture(0, RotCGPack.textToRlFull(go.texture.get(0)));
+                int y = i % numRows;
+                int x = i / numRows;
+                GuiComponent.blit(stack, x*drawSize, y * drawSize, drawSize, drawSize, 0, 0, 16, 16, 16, 16);
+            }
+            stack.translate(-13, 3, 0);
+            stack.scale(3, 3, 1);
+            font.drawShadow(stack, "{", 0, 0, 0xff_ff_ff_ff);
+            stack.translate(Math.ceil(numItems / (float)numRows)*drawSize/3f + 6, 0, 0);
+            font.drawShadow(stack, "}", 0, 0, 0xff_ff_ff_ff);
+            stack.popPose();
+
             stack.popPose();
             data.increasePosY(35);
         }
